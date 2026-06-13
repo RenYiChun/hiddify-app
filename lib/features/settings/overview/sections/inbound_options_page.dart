385ec310eb75ce04ca68b188262609e9a6d38db3
veto9292 <veto9292@gmail.com>
2026-06-13 20:33:17 +0330

    feat(settings): implement LAN sharing functionality with password support and UI integration (adding to quick settings section)

--- lib/features/settings/overview/sections/inbound_options_page.dart
+++ lib/features/settings/overview/sections/inbound_options_page.dart
@@ -1,12 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:hiddify/core/localization/translations.dart';
-import 'package:hiddify/core/router/dialog/dialog_notifier.dart';
 import 'package:hiddify/features/settings/data/config_option_repository.dart';
+import 'package:hiddify/features/settings/widget/lan_sharing_tile.dart';
 import 'package:hiddify/features/settings/widget/preference_tile.dart';
 import 'package:hiddify/singbox/model/singbox_config_enum.dart';
 import 'package:hiddify/utils/utils.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
-import 'package:network_info_plus/network_info_plus.dart';
 
 class InboundOptionsPage extends HookConsumerWidget {
   const InboundOptionsPage({super.key});
@@ -82,23 +81,7 @@ class InboundOptionsPage extends HookConsumerWidget {
             validateInput: isPort,
             trailing: SwitchPreferenceWidget(preference: ConfigOptions.enableDirectPort),
           ),
-          SwitchListTile.adaptive(
-            title: Text(t.pages.settings.inbound.allowConnectionFromLan),
-            secondary: const Icon(Icons.share_rounded),
-            value: ref.watch(ConfigOptions.allowConnectionFromLan),
-            onChanged: (bool value) async {
-              await ref.read(ConfigOptions.allowConnectionFromLan.notifier).update(value);
-              if (value == true) {
-                final ip = await NetworkInfo().getWifiIP();
-                // final ipp = Networkinfo
-                if (ip == null) return;
-                final port = ref.read(ConfigOptions.mixedPort);
-                final link = '#profile-title: LAN only\nsocks://$ip:$port#LAN only';
-                final message = 'socks://$ip:$port';
-                await ref.read(dialogNotifierProvider.notifier).showQrCode(link, message: message);
-              }
-            },
-          ),
+          const LanSharingPreferenceWidget(),
         ],
       ),
     );
